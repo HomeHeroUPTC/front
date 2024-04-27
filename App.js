097ac7ece65ca84b0f2fb6da631e5eef81e739df
Sebas320lopz <sebas320lopez@hotmail.com>
2024-04-27 14:15:01 -0500

    Agrgue mi nombre para pruebas

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>DARCKRONOZ</Text>
+      <Text>Sebastian Lopez</Text>
+      <Text>Milos agrga el nombre aqui </Text>
       <StatusBar style="auto" />
     </View>
   );
